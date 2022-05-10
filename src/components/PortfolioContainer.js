c294875899c63064619193846ef69110cfb36fca
Alec Harvey <ruinhard@gmail.com>
2022-05-10 18:27:17 -0500

    added components and responsive tab switching

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,21 @@
+import React,{useState,useEffect} from "react";
+import NavBar from "./NavBar"
+import Home from "./pages/Home"
+export default function PortfolioContainer(){
+    const [CurrentPage,setCurrentPage]=useState("Home")
+    const ChangeCurrentPage=(page)=>{setCurrentPage(page)}
+    const RenderCurrentPage=()=>{
+        switch(CurrentPage){
+           case "Home":
+                return <Home/>
+            default:
+                return <Home/>
+        }
+    }
+    return(
+        <div>
+            <NavBar CurrentPage={CurrentPage} ChangeCurrentPage={ChangeCurrentPage} />
+            {RenderCurrentPage()}
+        </div>
+    );
+}
