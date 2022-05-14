f94768d251caffb811b017a718f298f22680e4eb
Alec Harvey <ruinhard@gmail.com>
2022-05-14 16:45:28 -0500

    added watch list to portfolio. then added resume downloader

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -8,7 +8,7 @@ export default function Portfolio(){
                 <div className="card-body h-75 p-3">
                     <p>
                         Bootleg is a website created by Alec, Chris, Alex, and Gerald. It is a full-stack app
-                        using MySQL, Node, Bootstrap, and Sequelize. Once the user signs in or signs up, it will present the user 
+                        using MySQL, Node, Handlebars, and Sequelize. Once the user signs in or signs up, it will present the user 
                         the option to upload an image with an caption. Upon doing so, it will send the image renamed with the caption
                         to the server to be compressed and then displayed on the main page.
                     </p>
@@ -17,6 +17,44 @@ export default function Portfolio(){
                             <img style={{height:"40px"}} src="./graphics/hyperlinkicon.png" alt="hyperlink icon"/>
                         </a>
                         <a href="https://github.com/AlecRuin/Bootleg"  target={"_blank"} rel="noreferrer">
+                            <img style={{height:"40px",width:"40px"}} src="./graphics/githublogo.png" alt="hyperlink icon"/>
+                        </a>
+                    </div>
+                </div>   
+            </div>
+
+            <div className="card w-25 h-75 d-flex flex-column  my-2 mx-1">
+                <img src="./graphics/blogpostsnapshot.png" className="card-img-top h-25 d-flex"alt="Bootleg snapshot"/>
+                <div className="card-body h-75 p-3">
+                    <p>
+                        As a test of skill, I created Blog Posts. Its a full stack application using Node, Handlebars, Express, MySQL, and Node.
+                        Once a user signs up or logs in, it will allow the user to create simple blog posts with a title and a body. They may visit the dashboard
+                        and view all their previous posts and delete them if need be. 
+                    </p>
+                    <div className="postion-absolute start-50">
+                        <a href="https://group-project-bootleg.herokuapp.com/" target={"_blank"} rel="noreferrer">
+                            <img style={{height:"40px"}} src="./graphics/hyperlinkicon.png" alt="hyperlink icon"/>
+                        </a>
+                        <a href="https://github.com/AlecRuin/Bootleg"  target={"_blank"} rel="noreferrer">
+                            <img style={{height:"40px",width:"40px"}} src="./graphics/githublogo.png" alt="hyperlink icon"/>
+                        </a>
+                    </div>
+                </div>   
+            </div>
+            
+            <div className="card w-25 h-75 d-flex flex-column  my-2 mx-1">
+                <img src="./graphics/watchlistsnapshot.png" className="card-img-top h-25 d-flex"alt="Bootleg snapshot"/>
+                <div className="card-body h-75 p-3">
+                    <p>
+                        Movie Watchlist is a simple site utilizing the Watchmode api. Upon searching a movie, the API will return all streaming options of said movie. It will 
+                        also sort the streaming options based on price, on demand, 1 time purchase, and even give a hyperlin to purchase it on the site. It will then save all this info
+                        as a JSON object within the localstorage to prevent spamming the API key. I created the Javascript whilst the rest of my team developed the front end with Bulma
+                    </p>
+                    <div className="postion-absolute start-50">
+                        <a href="https://joetroester.github.io/Movie_Watchlist/" target={"_blank"} rel="noreferrer">
+                            <img style={{height:"40px"}} src="./graphics/hyperlinkicon.png" alt="hyperlink icon"/>
+                        </a>
+                        <a href="https://github.com/JoeTroester/Movie_Watchlist"  target={"_blank"} rel="noreferrer">
                             <img style={{height:"40px",width:"40px"}} src="./graphics/githublogo.png" alt="hyperlink icon"/>
                         </a>
                     </div>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,7 +3,11 @@ import React from "react";
 export default function Resume(){
     return (
         <div>
-            
+            <a href="./graphics/Resume.pdf" download={true}>
+                <button className="btn btn-primary m-5">
+                    Download resume
+                </button>
+            </a>
         </div>
     )
 }
